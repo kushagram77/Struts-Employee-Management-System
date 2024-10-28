547f0ce19ec35a4b9cbd520e180cbaf606432620
kushagram77 <107687008+kushagram77@users.noreply.github.com>
2024-10-28 12:11:59 +0530

    Update UserDAO.java

--- src/main/java/com/yash/strutsdemo2/dao/UserDAO.java
+++ src/main/java/com/yash/strutsdemo2/dao/UserDAO.java
@@ -1,69 +1,13 @@
 package com.yash.strutsdemo2.dao;
 
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 import com.yash.strutsdemo2.model.User;
-import com.yash.strutsdemo2.util.DBConnection;
 
-public class UserDAO {
-    public void addUser(User user) {
-        try (Connection con = DBConnection.getConnection()) {
-            PreparedStatement ps = con.prepareStatement(
-                    "INSERT INTO users (name, email, phone) VALUES (?, ?, ?)"
-            );
-            ps.setString(1, user.getName());
-            ps.setString(2, user.getEmail());
-            ps.setString(3, user.getPhone());
-            ps.executeUpdate();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+import java.util.List;
 
-    public List<User> getAllUsers() {
-        List<User> users = new ArrayList<>();
-        try (Connection con = DBConnection.getConnection()) {
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM users");
-            while (rs.next()) {
-                User user = new User();
-                user.setId(rs.getInt("id"));
-                user.setName(rs.getString("name"));
-                user.setEmail(rs.getString("email"));
-                user.setPhone(rs.getString("phone"));
-                users.add(user);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return users;
-    }
+public interface UserDAO {
 
-    public void updateUser(User user) {
-        try (Connection con = DBConnection.getConnection()) {
-            PreparedStatement ps = con.prepareStatement(
-                    "UPDATE users SET name=?, email=?, phone=? WHERE id=?"
-            );
-            ps.setString(1, user.getName());
-            ps.setString(2, user.getEmail());
-            ps.setString(3, user.getPhone());
-            ps.setInt(4, user.getId());
-            ps.executeUpdate();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void deleteUser(int id) {
-        try (Connection con = DBConnection.getConnection()) {
-            PreparedStatement ps = con.prepareStatement(
-                    "DELETE FROM users WHERE id=?"
-            );
-            ps.setInt(1, id);
-            ps.executeUpdate();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    void addUser(User user);
+    List<User> getAllUsers();
+    void updateUser(User user);
+    void deleteUser(int id);
 }
